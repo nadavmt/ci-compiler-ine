0aa46ad7db2cc76ab183e0436e822e2d25b1ebd3
nivstein <nivstein@30d398bf-167b-9ab7-bd56-e7439f1d9f73>
2010-11-06 18:26:13 +0000

    Compiler.java/Token.java first implementation for Exercise #1

--- src/IC/Compiler.java
+++ src/IC/Compiler.java
@@ -1,8 +1,81 @@
 package IC;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import IC.Parser.Lexer;
+import IC.Parser.LexicalError;
+import IC.Parser.Token;
+import IC.Parser.sym;
+
 public class Compiler
 {
     public static void main(String[] args)
     {
+    	if (args.length != 1)
+    	{
+    		System.err.println("Usage: java IC.Compiler <file.ic>");
+    		return;
+    	}
+    	
+    	File file = new File(args[0]);
+    	FileInputStream fis = null;
+    	
+    	try
+    	{
+    		fis = new FileInputStream(file);
+    	}
+    	catch (FileNotFoundException e)
+    	{
+    		System.err.println("Unable to find the specified file.");
+    		return;
+    	}
+    	
+    	if (fis != null)
+    	{
+    		try
+    		{
+	    		Lexer lexer = new Lexer(fis);
+	    		Token token = lexer.next_token();
+	    		
+	    		do
+	    		{
+	    			int line = token.lineNumber;
+	    			
+	    			String tokenName = sym.tokenName(token.sym);
+	    			String tokenVal = (token.value != null) ? "(" + token.value.toString() + ")" : "";
+	    			
+	    			System.out.println(line + ": " + tokenName + tokenVal);
+	    			
+	    			token = lexer.next_token();
+	    		}
+	    		while (token.sym != sym.EOF);
+    		}
+    		catch (LexicalError e)
+    		{
+    			int lineNumber = e.getLineNumber();
+    			
+    			String line = (lineNumber != -1) ? lineNumber + ": " : "";
+    			String message = e.getMessage();
+    			
+    			System.out.println(line + "Lexical error: " + message);
+    			return;
+    		}
+    		catch (IOException e)
+    		{
+    			System.err.println("Unknown IO exception.");
+    			return;
+    		}
+    		finally
+    		{
+    			try
+    			{
+    				fis.close();
+    			}
+    			catch (IOException e) {}
+    		}
+    	}
     }
 }
--- src/IC/Parser/Token.java
+++ src/IC/Parser/Token.java
@@ -2,9 +2,22 @@ package IC.Parser;
 
 import java_cup.runtime.Symbol;
 
-public class Token extends Symbol {
-    public Token(int id, int line) {
+public class Token extends Symbol
+{
+	public int lineNumber;
+	
+    public Token(int line, int id)
+    {
         super(id, null);
+        
+        this.lineNumber = line;
+    }
+    
+    public Token(int line, int id, Object val)
+    {
+    	super(id, val);
+    	
+    	this.lineNumber = line;
     }
 }
 
