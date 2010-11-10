74a4d22ad53d6bad91e360d40b215e613c40681b
eylony@gmail.com <eylony@gmail.com@30d398bf-167b-9ab7-bd56-e7439f1d9f73>
2010-11-10 20:21:47 +0000

    added 3 tests and some comments to LexicalError.java

--- trunk/src/IC/Parser/LexicalError.java
+++ trunk/src/IC/Parser/LexicalError.java
@@ -2,22 +2,29 @@ package IC.Parser;
 
 public class LexicalError extends Exception
 {
+	// The warning told me to put this here...
 	private static final long serialVersionUID = 1L;
 	
+	// will save the line number of the error
 	private int lineNumber = -1;
 	
+	// ctor with no line number
 	public LexicalError(String message)
 	{
 		super(message);
 	}
 	
+	// ctor
     public LexicalError(int line, String message)
     {
+    	// call exception's ctor
     	super(message);
     	
+    	// save line number
     	this.lineNumber = line;
     }
-    
+
+    // returns the line number which the error was in
     public int getLineNumber()
     {
     	return lineNumber;
